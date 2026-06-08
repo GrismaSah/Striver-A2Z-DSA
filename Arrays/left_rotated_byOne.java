464472a9ad3ec2366d2e74d34530817b868c15df
Gr1sm4 <23btrcn082@jainuniversity.ac.in>
2026-06-08 23:13:47 +0530

    added one more qn

--- Arrays/left_rotated_byOne.java
+++ Arrays/left_rotated_byOne.java
@@ -8,3 +8,40 @@ class Solution {
         nums[n - 1] = temp;
     }
 }
+
+// left rotated array by k places
+class Solution {
+
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public void rotateArray(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+        reverse(arr, 0, n - 1);
+
+        // int n = arr.length;
+
+        // for(int i = 0; i < (k%n); i++){
+        // int temp = arr[0];
+        // for(int j=1; j<n; j++){
+        // arr[j-1] = arr[j];
+        // }
+        // arr[n-1] = temp;
+        // }
+
+        // optimal solution..
+
+    }
+}
